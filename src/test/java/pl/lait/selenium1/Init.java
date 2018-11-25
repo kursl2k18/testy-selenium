294cb76e83c0bd445d522fe01b1efa66b266337f
adam <adam@192.168.0.101>
2018-11-25 17:50:42 +0100

    Pełny test logowania wg modelu z uzyciem JUnit

--- src/test/java/pl/lait/selenium1/Init.java
+++ src/test/java/pl/lait/selenium1/Init.java
@@ -12,23 +12,36 @@ public class Init {
 	static WebDriver driver;
 	
 	public static WebDriver getDriver() {
-		
+		System.out.println("wewnatrz getDriver");
 		System.setProperty("webdriver.chrome.driver", "/Users/adam/Documents/LAIT/chromedriver");
     	DesiredCapabilities cap = DesiredCapabilities.chrome();
+    	System.out.println("### decyduję czy wykonać new ChromeDriver");
     	
-    	driver = new ChromeDriver();
-    	driver.get("http://newtours.demoaut.com");
-    	
+    	if(driver==null) {
+    		System.out.println("### TAK ###");
+    		driver = new ChromeDriver();
+        	System.out.println("get http...");
+        	driver.get("http://newtours.demoaut.com");
+        	return driver;
+    	} else {
+    		System.out.println("### NIE ###");
+    		return driver;
+    	}
     	//driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    	sleep(2);
-    	
-    	return driver;
+    	//sleep(2);
 	}
 	
 	public static void endTest() {
+		System.out.println("wewnatrz endTest");
 		driver.quit();
+		driver = null;
+		System.out.println("po endTest");
 	}
 	
+	/**
+	 * Metoda wstrzymuje wykonywanie programu na X sekund
+	 * @param seconds
+	 */
 	public static void sleep(int seconds) {
 		try {
 			Thread.sleep(1000 * seconds);
--- src/test/java/pl/lait/selenium1/LoginTest.java
+++ src/test/java/pl/lait/selenium1/LoginTest.java
@@ -1,9 +1,11 @@
 package pl.lait.selenium1;
 
 import org.junit.*;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.Select;
 
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class LoginTest {
 
 	WebDriver driver = null;
@@ -13,16 +15,34 @@ public class LoginTest {
 		driver = Init.getDriver();
 	}
 	
+	@Ignore
 	@Test
 	public void clickOnMenuItems() {
 		driver.findElement(By.linkText("SIGN-ON")).click();
 		driver.findElement(By.linkText("REGISTER")).click();
 		driver.findElement(By.linkText("SUPPORT")).click();
 		driver.findElement(By.linkText("CONTACT")).click();
+	}
+	
+	@Ignore
+	@Test
+	public void login() {
 		driver.findElement(By.linkText("SIGN-ON")).click();
     	driver.findElement(By.name("userName")).sendKeys("adamdz");
     	driver.findElement(By.name("password")).sendKeys("qwe123");
     	driver.findElement(By.name("login")).click();
+    	
+    	String round_trip = "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/b/font/input[1]";
+    	String one_way =    "/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/b/font/input[2]";
+    	
+    	driver.findElement(By.xpath(round_trip)).click();
+    	driver.findElement(By.xpath(one_way)).click();
+    	
+    	Select pass_count = new Select(driver.findElement(By.name("passCount")));
+    	pass_count.selectByValue("1");
+    	
+    	//Init.sleep(5);
+    		
 	}
 	
 	@After
